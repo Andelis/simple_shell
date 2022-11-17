e6cd020e5fd079080390a8edbcce47c19c8cc506
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-11-17 09:41:24 -0500

    Create supporting programs

---
+++ shell.c
@@ -0,0 +1,95 @@
+#include "shell.h"
+
+/**
+ * sig_handler - check when 'Ctrl C' is pressed
+ * @sig_num: int
+ *
+ */
+void sig_handler(int sig_num)
+{
+	if (sig_num == SIGINT)
+	{
+		_puts("\n$simple_shell$ ");
+	}
+}
+
+/**
+ * _EOF -handles the end of file
+ * @len: return value of getline function
+ * @buff: buffer
+ *
+ */
+void _EOF(int len, char *buff)
+{
+	(void)buff;
+	if (len == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			_puts("\n");
+			free(buff);
+		}
+		exit(0);
+	}
+}
+
+/**
+ * _isatty - verif if terminal
+ *
+ */
+void _isatty(void)
+{
+	if (isatty((STDIN_FILENO)))
+	{
+		_puts("$simple_shell$ ");
+	}
+}
+
+/**
+ * main - shell
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	ssize_t len = 0;
+	char *buff = NULL, *value, *pathname, **arv;
+	size_t size = 0;
+	list_path *head = '\0';
+	void (*f)(char **);
+
+	signal(SIGINT, sig_handler);
+	while (len != EOF)
+	{
+		_isatty();
+		len = getline(&buff, &size, stdin);
+		_EOF(len, buff);
+		arv = splitstring(buff, " \n");
+		if (!arv || !arv[0])
+			execute(arv);
+		else
+		{
+			value = _getenv("PATH");
+			head = linkpath(value);
+			pathname = _which(arv[0], head);
+			f = checkbuild(arv);
+			if (f)
+			{
+				free(buff);
+				f(arv);
+			}
+			else if (!pathname)
+				execute(arv);
+			else if (pathname)
+			{
+				free(arv[0]);
+				arv[0] = pathname;
+				execute(arv);
+			}
+		}
+	}
+	free_list(head);
+	freearv(arv);
+	free(buff);
+	return (0);
+}
